f6bfd862326c18db9ab8764c323fbaed7b9ec630
Yusuf Eko Anggoro <anggoroekoyusuf@gmail.com>
2025-03-22 20:47:54 +0700

    update response

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello from Dockerized Node.js on AWS!");
+  res.send("Hello from Dockerized Node.js on AWS! - 20:47 22/03/2025");
 });
 
 app.listen(PORT, () => {
